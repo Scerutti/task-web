b05a09127bcaf493ac268b7d9a7c5eaa9d5551e6
Sebastián Cerutti <softsys95@gmail.com>
2023-06-14 21:24:18 -0300

    first feat: initializing tailwind and config the frontend proyect

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="bg-zinc-900 h-screen text-white flex item-center justify-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1 className="text-3xl font-bold text-center block my-2">Aplicacion de Tareas</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
